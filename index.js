c9f79b319c0ab53503f94ce4295f2545fc9bf01f
Katelyn Rodrigues <katelyn.rodrigues1@gmail.com>
2018-07-26 20:57:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 function scuberGreetingForFeet(scuberGreetingForFeettest){
-  if distance<=400{
+  if scuberGreetingForFeettest<=400{
     return "This one is on me!"
   }
 }
